911f3f773c805e4dcc8473a3567f5d4f36861716
Nurbek Kuspanov <nurbek@inbox.ru>
2020-07-22 20:40:48 +0400

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,29 @@
+html = b"""
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Bash.im random quote</title>
+  </head>
+  <body>
+    <script type="text/javascript" src="https://bash.im/forweb/?u"></script>
+    <button id="btn" type="button" class="btn btn-outline-dark" onclick="myfunction()">Random</button>
+    <script>function myfunction() {setTimeout(function(){ location.reload(); }, 5000)}</script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+"""
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    #data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/html'),
+    ]
+    start_response(status, response_headers)
+    return iter([html])
