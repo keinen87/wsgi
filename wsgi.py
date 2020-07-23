b421ccb5f55b7796eb982fcc9be0125728319f72
Nurbek Kuspanov <nurbek@inbox.ru>
2020-07-23 17:27:24 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,16 +11,12 @@ html = b"""
     <script type="text/javascript" src="https://bash.im/forweb/?u"></script>
     <button id="btn" type="button" class="btn btn-outline-dark" onclick="myfunction()">Random</button>
     <script>function myfunction() {setTimeout(function(){ location.reload(); }, 5000)}</script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
 """
 
 def app(environ, start_response):
     """Simplest possible application object"""
-    #data = b'Hello, World!\n'
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/html'),
