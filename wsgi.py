3c0485c80445f69857b0b99061e5ec81c78897ab
Nurbek Kuspanov <nurbek@inbox.ru>
2020-07-23 23:27:01 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,14 @@
-html = b"""
+HTML = """
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Bash.im random quote</title>
+    <link rel="stylesheet" href=./static/styles.css>
+    <title>Узнать ip</title>
   </head>
   <body>
-    <script type="text/javascript" src="https://bash.im/forweb/?u"></script>
-    <button id="btn" type="button" class="btn btn-outline-dark" onclick="myfunction()">Random</button>
-    <script>function myfunction() {setTimeout(function(){ location.reload(); }, 5000)}</script>
+   <p id="content">Ваш ip адрес: {}</p>
   </body>
 </html>
 """
@@ -22,4 +20,5 @@ def app(environ, start_response):
         ('Content-type', 'text/html'),
     ]
     start_response(status, response_headers)
-    return iter([html])
+    html_as_bytes = HTML.format(environ["HTTP_X_REAL_IP"]).encode('utf-8')
+    return iter([html_as_bytes])
